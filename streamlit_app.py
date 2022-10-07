27ce3f8f777ee2e47dfc6d1372d8b5bb81df6da7
Fil <fica1991@yahoo.com>
2022-10-07 10:07:16 +0200

    Update streamlit_app.py
    fruity table json and normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# fruity table json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# normalize 
+streamlit.dataframe(fruityvice_normalized)
